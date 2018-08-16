8256606ae6f2ed38afed09f5ed4b59766b8e8240
magicalne <magicalne@gmail.com>
2018-08-16 14:44:55 +0800

    slow down subscription

--- exchange/src/main/java/io/magicalne/smym/exchanges/HuobiExchange.java
+++ exchange/src/main/java/io/magicalne/smym/exchanges/HuobiExchange.java
@@ -47,19 +47,21 @@ public class HuobiExchange {
     public void createOrderBook(Set<String> symbols, int size) {
         this.orderBookMap = new ConcurrentHashMap<>(symbols.size() / 3 * 4);
         this.orderBookSize = size;
+        UniverseApiCallback<DepthResponse> callback = new UniverseApiCallback<DepthResponse>() {
+            @Override
+            public void onResponse(DepthResponse depth) {
+                String ch = depth.getCh();
+                String symbolFromTopic = getSymbolFromTopic(ch);
+                updateOrderBook(symbolFromTopic, depth.getTick());
+            }
+        };
         for (String symbol : symbols) {
-            UniverseApiCallback<DepthResponse> callback = new UniverseApiCallback<DepthResponse>() {
-                @Override
-                public void onResponse(DepthResponse depth) {
-                    String ch = depth.getCh();
-                    String symbolFromTopic = getSymbolFromTopic(ch);
-                    updateOrderBook(symbolFromTopic, depth.getTick());
-                }
-            };
-            DepthRequest req = new DepthRequest();
-            req.setType("step5");
-            req.setSymbol(symbol);
             this.webSocketClient.onDepthEvent(symbol, callback);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                log.error("Thread sleep with interrupted exception.", e);
+            }
         }
         log.info("Create {} market order books.", symbols.size());
     }
